1f1bc6172a00f9fe79c3d0915b5d1e8cb3efe87b
EthanJWright <ewright362@gmail.com>
2019-11-18 07:50:46 -0700

    Adding package manager installs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Install Fisher (fish package manager)
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+# Install Plug (nvim / vim package manager)
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 mkdir -p ~/.config/nvim
 cp nvim/init.vim ~/.config/nvim
 
